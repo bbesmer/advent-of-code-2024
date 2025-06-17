2fb3de6699c62e5190ea139fe01982726757ba23
Beat Besmer <beatbesmer@hotmail.com>
2025-06-17 20:50:56 +0000

    Work on day_12

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,12 +1,11 @@
 fn main() {
-    println!("Hello, world!");
     let garden = "AAAA
 BBCD
 BBCC
 EEEC
 ";
     let (width, height, garden) = garden_from_string(garden.to_owned());
-    let price = calculate_fence_price(width, height, garden);
+    let price = calculate_fence_price(width, height, &garden);
     println!("Price={}", price)
 }
 
@@ -18,22 +17,37 @@ fn garden_from_string(garden: String) -> (usize, usize, Vec<char>) {
         if c != '\n' {
             length += 1;
             resutl.push(c);
-        }
-        else {
+        } else {
             height += 1
         }
     }
     return (length / height, height, resutl);
 }
 
-fn calculate_fence_price(width: usize, height: usize, garden: Vec<char>) -> usize {
+fn calculate_fence_price(width: usize, height: usize, garden: &Vec<char>) -> u32 {
     let mut visited: Vec<u8> = vec![0; width * height];
+    let total_price = 0;
     // print_matrix(width, garden);
-    // print_matrix(width, visited);
-    0
+    print_matrix(width, &visited);
+    while let Some(index) = get_non_visited_plot(&visited) {
+        let letter = garden[index];
+        let mut fencesForRegion: Vec<u8> = vec![0; width * height];
+        visited[index] = 1; // TODO calculate price for region
+    }
+    print_matrix(width, &visited);
+    return total_price;
 }
 
-fn print_matrix<T>(width: usize, vector: Vec<T>)
+fn get_non_visited_plot(visited: &Vec<u8>) -> Option<usize> {
+    for (i, x) in visited.iter().enumerate() {
+        if *x == 0 {
+            return Some(i);
+        }
+    }
+    return None;
+}
+
+fn print_matrix<T>(width: usize, vector: &Vec<T>)
 where
     T: std::fmt::Display,
 {
