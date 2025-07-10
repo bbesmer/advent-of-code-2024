c32464514634150768972e7444cf31bf3a465bbd
Beat Besmer <beatbesmer@hotmail.com>
2025-07-10 08:14:54 +0000

    Solve day_12 part 2

---
+++ day_12/src/garden.rs
@@ -0,0 +1,117 @@
+#[derive(Debug)]
+pub struct Garden {
+    pub(crate) width: usize,
+    pub(crate) height: usize,
+    cells: Vec<char>,
+}
+
+impl Garden {
+    pub fn new(garden: String) -> Self {
+        let mut length = 0;
+        let mut height = 0;
+        let mut cells = vec![];
+        for c in garden.chars() {
+            if c != '\n' {
+                length += 1;
+                cells.push(c);
+            } else {
+                height += 1
+            }
+        }
+        let width = length / height;
+        Self {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn get(&self, x: usize, y: usize) -> Option<char> {
+        if x < self.width && y < self.height {
+            Some(self.cells[y * self.width + x])
+        } else {
+            None
+        }
+    }
+
+    pub fn get_neighbor(&self, x: usize, y: usize, dx: isize, dy: isize) -> Option<char> {
+        let nx = x as isize + dx;
+        let ny = y as isize + dy;
+        if nx >= 0 && ny >= 0 && (nx as usize) < self.width && (ny as usize) < self.height {
+            self.get(nx as usize, ny as usize)
+        } else {
+            None
+        }
+    }
+}
+
+pub struct Plot<'a> {
+    garden: &'a Garden,
+    pub(crate) x: usize,
+    pub(crate) y: usize,
+    pub(crate) letter: char,
+}
+
+impl<'a> Plot<'a> {
+    pub fn new(garden: &'a Garden, x: usize, y: usize) -> Self {
+        let letter =  match garden.get(x, y) {
+            None => panic!("Implementation error, plot does not exist"),
+            Some(l) => l
+        };
+        Self {
+            garden,
+            x,
+            y,
+            letter,
+        }
+    }
+
+    pub fn extends_top(&self) -> bool {
+        match self.garden.get_neighbor(self.x, self.y, 0, -1) {
+            None => false,
+            Some(other_letter) => self.letter == other_letter,
+        }
+    }
+    pub fn extends_top_right(&self) -> bool {
+        match self.garden.get_neighbor(self.x, self.y, 1, -1) {
+            None => false,
+            Some(other_letter) => self.letter == other_letter,
+        }
+    }
+    pub fn extends_right(&self) -> bool {
+        match self.garden.get_neighbor(self.x, self.y, 1, 0) {
+            None => false,
+            Some(other_letter) => self.letter == other_letter,
+        }
+    }
+    pub fn extends_bottom_right(&self) -> bool {
+        match self.garden.get_neighbor(self.x, self.y, 1, 1) {
+            None => false,
+            Some(other_letter) => self.letter == other_letter,
+        }
+    }
+    pub fn extends_bottom(&self) -> bool {
+        match self.garden.get_neighbor(self.x, self.y, 0, 1) {
+            None => false,
+            Some(other_letter) => self.letter == other_letter,
+        }
+    }
+    pub fn extends_bottom_left(&self) -> bool {
+        match self.garden.get_neighbor(self.x, self.y, -1, 1) {
+            None => false,
+            Some(other_letter) => self.letter == other_letter,
+        }
+    }
+    pub fn extends_left(&self) -> bool {
+        match self.garden.get_neighbor(self.x, self.y, -1, 0) {
+            None => false,
+            Some(other_letter) => self.letter == other_letter,
+        }
+    }
+    pub fn extends_top_left(&self) -> bool {
+        match self.garden.get_neighbor(self.x, self.y, -1, -1) {
+            None => false,
+            Some(other_letter) => self.letter == other_letter,
+        }
+    }
+}
--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,11 +1,14 @@
-use std::{fs::File, io::Read, path::Path, time::Instant};
+mod garden;
+
+use std::{fmt::Display, fs::File, io::Read, path::Path, time::Instant};
+
+use crate::garden::{Garden, Plot};
 
 fn main() {
     let path = Path::new("./input.txt");
-    let display = path.display();
 
     let mut file = match File::open(&path) {
-        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Err(why) => panic!("couldn't open {}: {}", path.display(), why),
         Ok(file) => file,
     };
 
@@ -13,18 +16,20 @@ fn main() {
     if let Err(why) = file.read_to_string(&mut garden) {
         panic!("couldn't read {}: {}", path.display(), why);
     }
-//     let garden = "OOOOO
-// OXOXO
-// OOOOO
-// OXOXO
-// OOOOO
+    
+    // let garden = "OOOOO\nOXOXO\nOOOOO\nOXOXO\nOOOOO\n";
+//     let garden = "AAAA
+// BBCD
+// BBCC
+// EEEC
 // ";
-    let iterations = 1;
+    let discounted = true;
+    let iterations = 5;
     let start = Instant::now();
     let mut price = 0;
     for _ in 0..iterations {
-        let (width, height, garden) = garden_from_string(garden.to_owned());
-        price = calculate_total_fence_price(width, height, &garden);
+        let garden = Garden::new(garden.to_owned());
+        price = calculate_total_fence_price(&garden, discounted);
     }
     let duration = start.elapsed();
     println!("Time per iteration: {:?}", duration / iterations);
@@ -32,102 +37,128 @@ fn main() {
     assert_eq!(1370258, price);
 }
 
-fn garden_from_string(garden: String) -> (usize, usize, Vec<char>) {
-    let mut length = 0;
-    let mut height = 0;
-    let mut resutl = vec![];
-    for c in garden.chars() {
-        if c != '\n' {
-            length += 1;
-            resutl.push(c);
-        } else {
-            height += 1
-        }
-    }
-    return (length / height, height, resutl);
-}
-
-fn calculate_total_fence_price(width: usize, height: usize, garden: &Vec<char>) -> u32 {
-    let mut visited: Vec<bool> = vec![false; width * height];
+fn calculate_total_fence_price(garden: &Garden, discounted: bool) -> u32 {
+    let mut visited: Vec<bool> = vec![false; garden.width * garden.height];
     let mut total_price = 0;
-    // print_matrix(width, &garden);
-    while let Some(index) = get_non_visited_plot(&visited) {
+    while let Some(plot) = get_non_visited_plot(&visited, &garden) {
         let regional_price =
-            calculate_regional_fence_price(width, height, garden, &mut visited, index);
+            calculate_regional_fence_price(&garden, &mut visited, plot, discounted);
         total_price += regional_price;
     }
     return total_price;
 }
 
-fn get_non_visited_plot(visited: &Vec<bool>) -> Option<usize> {
-    for (i, x) in visited.iter().enumerate() {
-        if !*x {
-            return Some(i);
+fn get_non_visited_plot<'a>(visited: &Vec<bool>, garden: &'a Garden) -> Option<Plot<'a>> {
+    for (i, &x) in visited.iter().enumerate() {
+        if !x {
+            return Some(Plot::new(&garden, i % garden.width, i / garden.width));
         }
     }
     return None;
 }
 
 fn calculate_regional_fence_price(
-    width: usize,
-    height: usize,
-    garden: &Vec<char>,
+    garden: &Garden,
     visited: &mut Vec<bool>,
-    index: usize,
+    plot: Plot,
+    discounted: bool
 ) -> u32 {
-    let letter = garden[index];
-    visited[index] = true;
-    let mut queue = vec![index];
+    visited[plot.x + plot.y*garden.width] = true;
+    let mut stack = vec![plot];
 
     let mut area = 0;
     let mut fences = 0;
-    while let Some(index) = queue.pop() {
-        let mut neighbors: Vec<usize> = vec![];
-        area += 1;
-        // left
-        if index % width == 0 {
-            fences += 1;
-        } else {
-            neighbors.push(index - 1);
-        }
-
-        // right
-        if index % width == width - 1 {
-            fences += 1;
-        } else {
-            neighbors.push(index + 1);
-        }
-
-        // top
-        if index / width == 0 {
-            fences += 1;
-        } else {
-            neighbors.push(index - width);
-        }
-
-        //bottom
-        if index / width == height - 1 {
-            fences += 1;
-        } else {
-            neighbors.push(index + width);
-        }
-
-        for neighbor in neighbors {
-            if garden[neighbor] != letter {
-                fences += 1;
-            } else if !visited[neighbor] {
-                visited[neighbor] = true;
-                queue.push(neighbor)
+    let mut inner_edges = 0;
+    let mut outer_edges = 0;
+    while let Some(plot) = stack.pop() {
+        // println!("{},{},{}", plot.letter, plot.x, plot.y);
+        let extends_left = plot.extends_left();
+        if extends_left {
+            if !visited[plot.x - 1 + plot.y*garden.width]{
+                visited[plot.x - 1  + plot.y*garden.width] = true;
+                stack.push(Plot::new(&garden, plot.x - 1, plot.y));
             }
         }
+
+        let extends_right = plot.extends_right();
+        if extends_right {
+            if !visited[plot.x + 1 + plot.y*garden.width] {
+                visited[plot.x + 1 + plot.y*garden.width] = true;
+                stack.push(Plot::new(&garden, plot.x + 1, plot.y));
+            }
+        }
+
+        let extends_top = plot.extends_top();
+        if extends_top {
+            if !visited[plot.x + plot.y*garden.width - garden.width] {
+                visited[plot.x + plot.y*garden.width - garden.width] = true;
+                stack.push(Plot::new(&garden, plot.x, plot.y - 1));
+            }
+        }
+        
+        let extends_bottom = plot.extends_bottom();
+        if extends_bottom {
+            if !visited[plot.x + plot.y*garden.width + garden.width] {
+                visited[plot.x + plot.y*garden.width + garden.width] = true;
+                stack.push(Plot::new(&garden, plot.x, plot.y + 1));
+            }
+        }
+
+        area += 1;
+        let fence_conditions = [!extends_top, !extends_right, !extends_left, !extends_bottom];
+        // println!("{:?}", fence_conditions);
+        fences += fence_conditions
+            .iter()
+            .fold(0, |acc, &b| if b { acc + 1 } else { acc });
+
+        let outer_edge_conditions = [
+            !extends_bottom & !extends_left,
+            !extends_left & !extends_top,
+            !extends_top & !extends_right,
+            !extends_right & !extends_bottom
+        ];
+        outer_edges += outer_edge_conditions
+            .iter()
+            .fold(0, |acc, &b| if b { acc + 1 } else { acc });
+        
+        let extends_top_left = plot.extends_top_left();
+        let extends_bottom_left = plot.extends_bottom_left();
+        let extends_top_right = plot.extends_top_right();
+        let extends_bottom_right = plot.extends_bottom_right();
+
+        let inner_edge_conditions = [
+            extends_bottom & extends_left & !extends_bottom_left,
+            extends_bottom & !extends_left & extends_bottom_left,
+            !extends_bottom & extends_left & extends_bottom_left,
+            extends_left & extends_top & !extends_top_left,
+            extends_left & !extends_top & extends_top_left,
+            !extends_left & extends_top & extends_top_left,
+            extends_top & extends_right & !extends_top_right,
+            extends_top & !extends_right & extends_top_right,
+            !extends_top & extends_right & extends_top_right,
+            extends_right & extends_bottom & !extends_bottom_right,
+            extends_right & !extends_bottom & extends_bottom_right,
+            !extends_right & extends_bottom & extends_bottom_right,
+        ];
+        inner_edges += inner_edge_conditions
+            .iter()
+            .fold(0, |acc, &b| if b { acc + 1 } else { acc });
+
     }
-    // println!("Region {}: Area={}, Fences={}", letter, area, fences);
-    return area * fences;
+    // println!("Region {}: Area={}, Fences={}, InnerEdges={}, OuterEdges={}", '?', area, fences, inner_edges, outer_edges);
+    let price;
+    if discounted {
+        price = area * (outer_edges + inner_edges / 3);
+    }
+    else {
+        price = area * fences;
+    }
+    return price;
 }
 
 fn _print_matrix<T>(width: usize, vector: &Vec<T>)
 where
-    T: std::fmt::Display,
+    T: Display,
 {
     for (i, x) in vector.iter().enumerate() {
         if i % width == 0 {
